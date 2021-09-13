bf89d4222768e27033d95cf62986119d916cbde7
Seniv Pavlo <senivpavlo016@gmail.com>
2021-09-13 15:43:06 +0300

    Update header tablet

---
+++ #src/js/_search_input.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByClassName(".category__item_1");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByClassName(".category__button")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
